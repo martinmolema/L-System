56526cb70cda921caaf58cf1fbd2c862ddc02b8d
Martin Molema <martin@molema.org>
2024-05-20 09:08:36 +0200

    Removed test files

--- src/app/classes/lsystem-jsonparameters.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LSystemJSONParameters } from './lsystem-jsonparameters';
-
-describe('LSystemJSONParameters', () => {
-  it('should create an instance', () => {
-    expect(new LSystemJSONParameters()).toBeTruthy();
-  });
-});
